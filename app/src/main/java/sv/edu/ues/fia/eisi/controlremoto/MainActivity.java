0a6460a219e260f37781d29da72ec58469ccd7a3
KevinCastellanos <kevincastellanosgaray@gmail.com>
2021-10-13 23:32:34 -0600

    evento de botones

--- app/src/main/java/sv/edu/ues/fia/eisi/controlremoto/MainActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/controlremoto/MainActivity.java
@@ -3,6 +3,7 @@ package sv.edu.ues.fia.eisi.controlremoto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -18,5 +19,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // referenciamos los controles
+        btnEnviar = findViewById(R.id.btnEnviar);
+        btnStop = findViewById(R.id.btnStop);
+        btnIzquierdo = findViewById(R.id.btnIzquierdo);
+        btnDerecho = findViewById(R.id.btnDerecho);
+        btnDesconectar = findViewById(R.id.btnDesconectar);
+
+        // eventos
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnIzquierdo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnDerecho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnDesconectar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
