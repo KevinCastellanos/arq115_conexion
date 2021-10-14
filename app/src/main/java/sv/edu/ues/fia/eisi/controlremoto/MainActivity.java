93d9b7b7bafa215aede0121ba86a70e05d665802
KevinCastellanos <kevincastellanosgaray@gmail.com>
2021-10-13 22:58:09 -0600

    captura de referencia de atributos

--- app/src/main/java/sv/edu/ues/fia/eisi/controlremoto/MainActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/controlremoto/MainActivity.java
@@ -3,8 +3,16 @@ package sv.edu.ues.fia.eisi.controlremoto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnEnviar;
+    ImageButton btnStop;
+    ImageButton btnIzquierdo;
+    ImageButton btnDerecho;
+    Button btnDesconectar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
